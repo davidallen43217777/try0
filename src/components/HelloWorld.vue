062bcf12582329af3a61cbaffeab9c364f340be8
Bernie <davidallen43217777@gmail.com>
2022-07-25 17:25:41 -0400

    wip: (Add: dist/assets/index.1f1caca4.css, dist/assets/index.33637822.js | Update dist/index.html, src/components/HelloWorld.vue; little edits | Delete: dist/assets/index.355a229a.js, dist/assets/index.35e49caf.css)

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -18,10 +18,7 @@ const count = ref(0)
   </div>
 
   <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    OJ 1234!
   </p>
   <p>
     Install
