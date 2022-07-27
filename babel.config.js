361c4f6b2e209ac0e1b15da03dfd07b1b638c8e1
Bernie <davidallen43217777@gmail.com>
2022-07-26 22:37:04 -0400

    wip: (Add: Dockerfile, babel.config.js, build/__tests__/lib.test.js, build/index.js, jest.config.js, lib/__tests__/lib.test.ts, lib/index.ts, okteto-k8s.yml, okteto.yml, package-lock.json, package.json, tsconfig.json | Modify .gitignore; minor changes)

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+
+module.exports = (api) => {
+  api.cache(true);
+  
+  return {
+    presets: [require("@babel/preset-env"), require("@babel/preset-typescript")],
+    plugins: [
+    ],
+  };
+}
+
---
+++ build/index.js
@@ -0,0 +1,25 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.port = exports.app = void 0;
+
+var _express = _interopRequireDefault(require("express"));
+
+var _dotenv = _interopRequireDefault(require("dotenv"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_dotenv["default"].config();
+
+var app = (0, _express["default"])();
+exports.app = app;
+var port = process.env.PORT;
+exports.port = port;
+app.get('/', function (req, res) {
+  res.send('Express + TypeScript Server');
+});
+app.listen(port, function () {
+  console.log("[server]: Server is running at http://localhost:".concat(port));
+});
